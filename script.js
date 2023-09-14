1184ab22c054e68944173f996c7e8ade6aed996f
Adrien-25 <61613165+Adrien-25@users.noreply.github.com>
2023-09-14 16:01:54 +0000

    Début de la bannière

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+
+const textContainer = document.querySelector('.section__text__p2');
+const texts = ["Développeur Web Fullstack", "Architecte du Web", "Maître du clavier"];
+let currentTextIndex = 0;
+let currentText = '';
+let isWriting = true;
+
+function writeAndEraseText() {
+  if (isWriting) {
+    currentText += texts[currentTextIndex][currentText.length];
+    textContainer.textContent = currentText;
+    if (currentText === texts[currentTextIndex]) {
+      isWriting = false;
+      setTimeout(writeAndEraseText, 2000); // Pause après l'écriture complète
+    } else {
+      setTimeout(writeAndEraseText, 100); // Délai entre les lettres
+    }
+  } else {
+    if (currentText === '') {
+      isWriting = true;
+      currentTextIndex = (currentTextIndex + 1) % texts.length;
+      setTimeout(writeAndEraseText, 0); // Délai avant de commencer à écrire la prochaine phrase
+    } else {
+      currentText = currentText.slice(0, -1);
+      textContainer.textContent = currentText;
+      setTimeout(writeAndEraseText, 50); // Délai entre les effacements de lettres
+    }
+  }
+}
+
+writeAndEraseText();
+
+// Toggle menu hamburger 
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
@@ -16,11 +49,11 @@ if (currentTheme === "dark") {
   setDarkMode();
 }
 
-btn.addEventListener("click", function () {
+btn.addEventListener("click", function() {
   setTheme();
 });
 
-btn2.addEventListener("click", function () {
+btn2.addEventListener("click", function() {
   setTheme();
 });
 
